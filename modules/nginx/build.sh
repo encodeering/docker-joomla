f2e2b6c3a5ab97ae4a1363e5147797149cd1b13e
Michael Clausen <encodeering@gmail.com>
2019-03-31 10:12:50 +0200

    provides nginx extension
    * image is based on the latest code from the docker cloud project
    
    NOTE
    
    * https://docs.joomla.org/Nginx#Configure_Nginx

---
+++ modules/nginx/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
+
+docker-pull "$REPOSITORY/nginx-$ARCH:1.14-alpine-sequel" "nginx:alpine"
+
+docker-build alpine
+
+docker-verify -V 2>&1 | dup | contains "nginx/1.14"
